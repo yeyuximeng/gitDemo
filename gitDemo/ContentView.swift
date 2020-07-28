8d8d030f6614c2d8a8a32cf4f5d4a84d233ac054
yeyuximeng <cxbyeyu@163.com>
2020-07-28 23:37:23 +0800

    init project

---
+++ gitDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitDemo
+//
+//  Created by 夜雨 on 2020/7/28.
+//  Copyright © 2020 夜雨. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!11")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
